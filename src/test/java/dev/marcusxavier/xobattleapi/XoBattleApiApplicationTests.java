3a07b083182deed18c9fb763c051ed6a09d88997
Marcus Xavier <marcusxavierr123@gmail.com>
2024-05-01 00:09:50 -0300

    chore: first commit

---
+++ src/test/java/dev/marcusxavier/xobattleapi/XoBattleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcusxavier.xobattleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XoBattleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
