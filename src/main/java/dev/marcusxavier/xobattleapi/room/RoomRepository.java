77bec2b548e5f3082e53e7db42925137ee9302d2
Marcus Xavier <marcusxavierr123@gmail.com>
2024-05-01 00:15:01 -0300

    refactor: improve repository code

--- src/main/java/dev/marcusxavier/xobattleapi/room/RoomRepository.java
+++ src/main/java/dev/marcusxavier/xobattleapi/room/RoomRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.repository.ListCrudRepository;
 import java.util.Optional;
 
 public interface RoomRepository extends ListCrudRepository<Room, String> {
-  public Room getRoomByRoomId(String roomId);
-
-  public Optional<Room> findByRoomId(String roomId);
+  Optional<Room> findByRoomId(String roomId);
 }
