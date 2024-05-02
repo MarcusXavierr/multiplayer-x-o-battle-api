5b420eef6f7e3128a4d5c217c67e26aa8e847d0f
Marcus Xavier <marcusxavierr123@gmail.com>
2024-05-01 22:08:10 -0300

    feat: creating the websocket work for a user messages another when join a room

---
+++ src/main/java/dev/marcusxavier/xobattleapi/onlinegame/GameController.java
@@ -0,0 +1,26 @@
+package dev.marcusxavier.xobattleapi.onlinegame;
+
+import dev.marcusxavier.xobattleapi.room.Room;
+import dev.marcusxavier.xobattleapi.room.RoomRepository;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@CrossOrigin
+public class GameController {
+  private final RoomRepository roomRepository;
+
+    public GameController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+  
+    @MessageMapping("/room-joined/{id}")
+    @SendTo("/topic/room-joined/{id}")
+    public boolean joinRoom(@DestinationVariable String id) {
+       return true;
+    }
+}
